4258910dc7f1399f1ae1ebd04619fe92843fe1d8
aw-andre <aw.andregerard@gmail.com>
2025-03-06 12:20:21 -0500

    feat: make Oil delete to trash

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -1,11 +1,17 @@
 {
-    plugins.oil.enable = true;
-    keymaps = [
-      {
-        mode = "n";
-        key = "-";
-        action.__raw = "require('oil').open";
-        options.desc = "Open File Explorer";
-      }
-    ];
+  plugins.oil = {
+    enable = true;
+    settings = {
+      delete_to_trash = true;
+      experimental_watch_for_changes = true;
+    };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "-";
+      action.__raw = "require('oil').open";
+      options.desc = "Open File Explorer";
+    }
+  ];
 }
