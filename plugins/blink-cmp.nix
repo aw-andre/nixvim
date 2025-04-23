853a37c9767cc64055c323b824c8d86507dab289
aw-andre <aw.andregerard@gmail.com>
2025-04-22 22:09:41 -0400

    feat!: remove blink.cmp ghost text

--- plugins/blink-cmp.nix
+++ plugins/blink-cmp.nix
@@ -12,12 +12,9 @@
       enable = true;
       setupLspCapabilities = true;
       settings = {
-        completion = {
-          documentation = {
-            auto_show = true;
-            auto_show_delay_ms = 0;
-          };
-          ghost_text.enabled = true;
+        completion.documentation = {
+          auto_show = true;
+          auto_show_delay_ms = 0;
         };
         signature.enabled = true;
         sources = {
