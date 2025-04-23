ca25bbd5f8794262606d83c78e5a27b3159c4d50
aw-andre <aw.andregerard@gmail.com>
2025-04-22 22:02:36 -0400

    feat: make Oil.nvim show hidden files

--- plugins/oil.nix
+++ plugins/oil.nix
@@ -4,14 +4,13 @@
     settings = {
       delete_to_trash = true;
       experimental_watch_for_changes = true;
+      view_options.show_hidden = true;
     };
   };
-  keymaps = [
-    {
-      mode = "n";
-      key = "-";
-      action.__raw = "require('oil').open";
-      options.desc = "Open File Explorer";
-    }
-  ];
+  keymaps = [{
+    mode = "n";
+    key = "-";
+    action.__raw = "require('oil').open";
+    options.desc = "Open File Explorer";
+  }];
 }
