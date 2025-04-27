ae60c69877bcce4dc9a8930efbb38793f2504861
aw-andre <aw.andregerard@gmail.com>
2025-04-27 16:52:05 -0400

    feat: change blink-cmp documentation borders

--- plugins/blink-cmp.nix
+++ plugins/blink-cmp.nix
@@ -15,6 +15,7 @@
         completion.documentation = {
           auto_show = true;
           auto_show_delay_ms = 0;
+          window.border = "rounded";
         };
         signature.enabled = true;
         sources = {
