657c4f1013847ead651b82e6665fa0562499a308
aw-andre <aw.andregerard@gmail.com>
2025-06-23 12:09:13 -0400

    fix!: remove buggy cppcheck

--- ft/c.nix
+++ ft/c.nix
@@ -3,7 +3,7 @@
     dap-lldb.enable = true;
     lsp.servers.clangd.enable = true;
     none-ls.sources = {
-      diagnostics.cppcheck.enable = true;
+      # diagnostics.cppcheck.enable = true;
       formatting.clang_format.enable = true;
     };
   };
