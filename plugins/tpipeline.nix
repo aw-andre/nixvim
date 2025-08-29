ecc462d36c12d4361e38f9f3e258114ab1e8fd9d
aw-andre <aw.andregerard@gmail.com>
2025-08-29 18:09:43 -0400

    feat!: remove Neovim statusline from tmux

--- plugins/tpipeline.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  # plugins.lualine = {
-  #   enable = true;
-  #   settings.options.theme = "gruvbox_dark";
-  # };
-
-  globals = { tpipeline_statusline = "%!tpipeline#stl#line()"; };
-  extraPlugins = with pkgs.vimPlugins; [ vim-tpipeline ];
-}
