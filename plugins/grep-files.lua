be0dfecdc580718dee40515418bb5443e4b9db95
aw-andre <aw.andregerard@gmail.com>
2025-08-30 15:28:00 -0400

    fix: live grep instead of grepping for word

--- plugins/grep-files.lua
+++ plugins/grep-files.lua
@@ -1,6 +1,6 @@
 function() 
   if vim.fn.argc() == 0 and vim.fn.len(vim.fn.expand('%')) == 0 and stdin == nil
   then
-      require('telescope.builtin').grep_string()
+      require('telescope.builtin').live_grep()
   end
 end
