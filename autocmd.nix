8a8027ad48725d132a8d6259f5b49b04bcebbf88
aw-andre <aw.andregerard@gmail.com>
2025-09-04 18:21:49 -0400

    feat: deconfigure terminal buffers

--- autocmd.nix
+++ autocmd.nix
@@ -28,19 +28,19 @@
       '';
       desc = "Configure help buffers";
     }
-    {
-      event = "TermOpen";
-      callback.__raw = ''
-        function()
-          vim.opt_local.relativenumber = true
-          vim.opt_local.number = true
-          vim.opt_local.conceallevel = 0
-          vim.opt_local.cursorline = false
-          vim.opt_local.signcolumn = "yes"
-        end
-      '';
-      desc = "Configure terminal buffers";
-    }
+    # {
+    #   event = "TermOpen";
+    #   callback.__raw = ''
+    #     function()
+    #       vim.opt_local.relativenumber = true
+    #       vim.opt_local.number = true
+    #       vim.opt_local.conceallevel = 0
+    #       vim.opt_local.cursorline = false
+    #       vim.opt_local.signcolumn = "yes"
+    #     end
+    #   '';
+    #   desc = "Configure terminal buffers";
+    # }
     {
       event = "CursorMoved";
       callback.__raw = ''
