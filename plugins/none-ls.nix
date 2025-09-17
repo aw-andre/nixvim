83cf7acba3456a3f4e956717a4f0bbdcad1d22ae
aw-andre <aw.andregerard@gmail.com>
2025-09-16 23:07:19 -0400

    fix: remove luasnip completion for null-ls

--- plugins/none-ls.nix
+++ plugins/none-ls.nix
@@ -3,27 +3,19 @@
     enable = true;
     # enableLspFormat = true;
     settings.updateInInsert = false;
-    sources.completion.luasnip.enable = true;
   };
-  keymaps = [
-    {
-      mode = [
-        "n"
-        "v"
-      ];
-      key = "=";
-      action.__raw = "vim.lsp.buf.format";
-      options = {
-        silent = true;
-        desc = "Format Buffer";
-      };
-    }
-  ];
-  autoCmd = [
-    {
-      event = "BufWritePre";
-      callback.__raw = "function () vim.lsp.buf.format() end";
-      desc = "Format when writing text";
-    }
-  ];
+  keymaps = [{
+    mode = [ "n" "v" ];
+    key = "=";
+    action.__raw = "vim.lsp.buf.format";
+    options = {
+      silent = true;
+      desc = "Format Buffer";
+    };
+  }];
+  autoCmd = [{
+    event = "BufWritePre";
+    callback.__raw = "function () vim.lsp.buf.format() end";
+    desc = "Format when writing text";
+  }];
 }
